3c1f037c031abf7fc0a32459d431de515a89f1a8
mehammedhasen <mehammedhasen@yahoo.com>
2024-01-18 12:05:57 -0800

    1-calcula.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """print the sum, difference, multiple and division of 10 and 5"""
+    """print the sum, dif, mul and div of 10 and 5"""
     from calculator_1 import add, sub, mul, div
-
     a = 10
     b = 5
-
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
